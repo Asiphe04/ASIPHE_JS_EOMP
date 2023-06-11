8b116d85400c4ea789b2e03327776a06b6a2c79b
Asiphe04 <asiphendimlana40@gmail.com>
2023-06-11 22:18:40 +0200

    layout

--- js/products.js
+++ js/products.js
@@ -1,287 +1,246 @@
-// Home page
 
-let topPicks = [
-    {
-      id: 1,
-      name: "",
-      img: "",
-      price: 500,
-      desc: ""
-    },
-    {
-        id: 2,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 3,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-    
-  ];
-  
-  let dispTopPicks = document.querySelector(".row");
-  topPicks.forEach((data) => {
-    dispTopPicks.innerHTML += `
-      <br>
-         
-          <div class="employees col-4">
-          <h3>${data.name}</h3>
-          <h4>${data.job}</h4>
-          <img src="${data.img}" class="img-fluid w-25">
-         
-          </div>`;
-  });
+//Balls
 
-//   PRODUCTS
-// T-SHIRTS
+let balls = [
+  {
+    id: 1,
+        img: "https://i.postimg.cc/Hx7hB4Rn/epl-ball.jpg",
+        itemName: "EPL Match Ball",
+        desc: "The 22/23 season match ball of the premier league",
+        price: 500, 
+  },
+  {
+    id: 2,
+        img: "https://i.postimg.cc/Hx7hB4Rn/epl-ball.jpg",
+        itemName: "EPL Match Ball",
+        desc: "The 22/23 season match ball of the premier league",
+        price: 500, 
+  },
+  {
+    id: 3,
+        img: "https://i.postimg.cc/Hx7hB4Rn/epl-ball.jpg",
+        itemName: "EPL Match Ball",
+        desc: "The 22/23 season match ball of the premier league",
+        price: 500, 
+  },
+  {
+    id: 4,
+        img: "https://i.postimg.cc/Hx7hB4Rn/epl-ball.jpg",
+        itemName: "EPL Match Ball",
+        desc: "The 22/23 season match ball of the premier league",
+        price: 500, 
+  },
+  {
+    id: 5,
+        img: "https://i.postimg.cc/Hx7hB4Rn/epl-ball.jpg",
+        itemName: "EPL Match Ball",
+        desc: "The 22/23 season match ball of the premier league",
+        price: 500, 
+  },
+  {
+    id: 6,
+        img: "https://i.postimg.cc/Hx7hB4Rn/epl-ball.jpg",
+        itemName: "EPL Match Ball",
+        desc: "The 22/23 season match ball of the premier league",
+        price: 500, 
+  }
+]
+let dispballs = document.querySelector(".balls");
 
+balls.forEach((data) => {
+  dispballs.innerHTML += `
+ 
+    <div class="top-picks-items col-3 container">
+      <div class="img-container">
+        <img src="${data.img}" alt="" class="top-picks-img">
+      </div>  
+      <p>${data.itemName}<br>R${data.price}</p>
+      <button class="btn-add-to-cart btn">Add to Cart</button>
+      <button class="btn-see-more btn" onclick="openPopup('popup1')">See more</button>
+    </div>  
+  </div>`;
+});
 
-let tShirts= [
-    {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-  ];
-  
-  let dispEmployees = document.querySelector(".row");
-  employees.forEach((data) => {
-    dispEmployees.innerHTML += `
-      <br>
-         
-          <div class="employees col-4">
-          <h3>${data.name}</h3>
-          <h4>${data.job}</h4>
-          <img src="${data.img}" class="img-fluid w-25">
-         
-          </div>`;
-  });
+//End of Soccer balls
+// Trophies
+let trophies = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500,
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500,
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500,
+  },
+  {
+    id: 4,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500,
+  },
+  {
+    id: 5,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500,
+  },
+  {
+    id: 6,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500,
+  },
+]
 
-//   SOCCER BOOTS
-let employees = [
-    {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-  ];
-  
-  let dispEmployees = document.querySelector(".row");
-  employees.forEach((data) => {
-    dispEmployees.innerHTML += `
-      <br>
-         
-          <div class="employees col-4">
-          <h3>${data.name}</h3>
-          <h4>${data.job}</h4>
-          <img src="${data.img}" class="img-fluid w-25">
-         
-          </div>`;
-  });
+let dispTrophies = document.querySelector(".trophies");
 
-//   TROPHIES
-let employees = [
-    {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-  ];
-  
-  let dispEmployees = document.querySelector(".row");
-  employees.forEach((data) => {
-    dispEmployees.innerHTML += `
-      <br>
-         
-          <div class="employees col-4">
-          <h3>${data.name}</h3>
-          <h4>${data.job}</h4>
-          <img src="${data.img}" class="img-fluid w-25">
-         
-          </div>`;
-  });
-//   MATCH BALLS
+trophies.forEach((data) => {
+  dispTrophies.innerHTML += `
+ 
+    <div class="top-picks-items col-3 container">
+      <div class="img-container">
+        <img src="${data.img}" alt="" class="top-picks-img">
+      </div>  
+      <p>${data.itemName}<br>R${data.price}</p>
+      <button class="btn-add-to-cart btn">Add to Cart</button>
+      <button class="btn-see-more btn" onclick="openPopup('popup1')">See more</button>
+    </div>  
+  </div>`;
+});
 
+let boots =[
+  {
+    id: 1,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 4,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 5,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 6,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
 
-let employees = [
+]
+let dispBoots = document.querySelector(".boots");
 
-    {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-      {
-        id: 1,
-        name: "",
-        img: "",
-        price: 500,
-        desc: ""
-      },
-  ];
-  
-  let dispEmployees = document.querySelector(".row");
-  employees.forEach((data) => {
-    dispEmployees.innerHTML += `
-      <br>
-         
-          <div class="employees col-4">
-          <h3>${data.name}</h3>
-          <h4>${data.job}</h4>
-          <img src="${data.img}" class="img-fluid w-25">
-         
-          </div>`;
-  });
+boots.forEach((data) => {
+  dispBoots.innerHTML += `
+ 
+    <div class="top-picks-items col-3 container">
+      <div class="img-container">
+        <img src="${data.img}" alt="" class="top-picks-img">
+      </div>  
+      <p>${data.itemName}<br>R${data.price}</p>
+      <button class="btn-add-to-cart btn">Add to Cart</button>
+      <button class="btn-see-more btn" onclick="openPopup('popup1')">See more</button>
+    </div>  
+  </div>`;
+});
+
+let tShirts = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 4,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 5,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+  {
+    id: 6,
+    img: "https://i.postimg.cc/SKzHdQq2/ucl-trophy.jpg",
+    itemName: "UCL",
+    desc: "The most valuable club trophy , even 99% of football players would say so.",
+    price: 500, 
+  },
+]
+let dispTshirts = document.querySelector(".tShirts");
+
+tShirts.forEach((data) => {
+  dispTshirts.innerHTML += `
+ 
+    <div class="top-picks-items col-3 container">
+      <div class="img-container">
+        <img src="${data.img}" alt="" class="top-picks-img">
+      </div>  
+      <p>${data.itemName}<br>R${data.price}</p>
+      <button class="btn-add-to-cart btn">Add to Cart</button>
+      <button class="btn-see-more btn" onclick="openPopup('popup1')">See more</button>
+    </div>  
+  </div>`;
+});
