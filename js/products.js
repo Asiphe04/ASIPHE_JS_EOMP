1bc8bb9a3cdf273135ef67e155676cf2215c7eda
Asiphe04 <asiphendimlana40@gmail.com>
2023-06-11 12:16:58 +0200

    home_layout_done

---
+++ js/products.js
@@ -0,0 +1,287 @@
+// Home page
+
+let topPicks = [
+    {
+      id: 1,
+      name: "",
+      img: "",
+      price: 500,
+      desc: ""
+    },
+    {
+        id: 2,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 3,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+    
+  ];
+  
+  let dispTopPicks = document.querySelector(".row");
+  topPicks.forEach((data) => {
+    dispTopPicks.innerHTML += `
+      <br>
+         
+          <div class="employees col-4">
+          <h3>${data.name}</h3>
+          <h4>${data.job}</h4>
+          <img src="${data.img}" class="img-fluid w-25">
+         
+          </div>`;
+  });
+
+//   PRODUCTS
+// T-SHIRTS
+
+
+let tShirts= [
+    {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+  ];
+  
+  let dispEmployees = document.querySelector(".row");
+  employees.forEach((data) => {
+    dispEmployees.innerHTML += `
+      <br>
+         
+          <div class="employees col-4">
+          <h3>${data.name}</h3>
+          <h4>${data.job}</h4>
+          <img src="${data.img}" class="img-fluid w-25">
+         
+          </div>`;
+  });
+
+//   SOCCER BOOTS
+let employees = [
+    {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+  ];
+  
+  let dispEmployees = document.querySelector(".row");
+  employees.forEach((data) => {
+    dispEmployees.innerHTML += `
+      <br>
+         
+          <div class="employees col-4">
+          <h3>${data.name}</h3>
+          <h4>${data.job}</h4>
+          <img src="${data.img}" class="img-fluid w-25">
+         
+          </div>`;
+  });
+
+//   TROPHIES
+let employees = [
+    {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+  ];
+  
+  let dispEmployees = document.querySelector(".row");
+  employees.forEach((data) => {
+    dispEmployees.innerHTML += `
+      <br>
+         
+          <div class="employees col-4">
+          <h3>${data.name}</h3>
+          <h4>${data.job}</h4>
+          <img src="${data.img}" class="img-fluid w-25">
+         
+          </div>`;
+  });
+//   MATCH BALLS
+
+
+let employees = [
+
+    {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+      {
+        id: 1,
+        name: "",
+        img: "",
+        price: 500,
+        desc: ""
+      },
+  ];
+  
+  let dispEmployees = document.querySelector(".row");
+  employees.forEach((data) => {
+    dispEmployees.innerHTML += `
+      <br>
+         
+          <div class="employees col-4">
+          <h3>${data.name}</h3>
+          <h4>${data.job}</h4>
+          <img src="${data.img}" class="img-fluid w-25">
+         
+          </div>`;
+  });
